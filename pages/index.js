fa260e805ec147f3a60df424d5f3474b5b051b15
Tony Moraes <idd_tmoraes@uolinc.com>
2023-11-28 17:58:50 -0300

    adicionando o prittier

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 function Home() {
-    return <h2>indo ali e um meme famoso mas o capitão limpesa e melhor porque e um super heroy igual ao ominhoca que e o charles maromba kkkkkkkkk</h2>
+  return (
+    <h2>
+      indo ali e um meme famoso mas o capitão limpesa e melhor porque e um super
+      heroy igual ao ominhoca que e o charles maromba kkkkkkkkk
+    </h2>
+  );
+}
+
+function teste() {
+  console.log("teste");
 }
 
 export default Home;
-  
