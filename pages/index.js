f51b33fe9f032c60e03ffa829fb108f3400bf0d7
Tony Moraes <idd_tmoraes@uolinc.com>
2023-11-28 11:38:53 -0300

    mensagem do benjamin

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h2>Benjamin!! Consegui colocar uma frase na internet!</h2>
+    return <h2>indo ali e um meme famoso mas o capitão limpesa e melhor porque e um super heroy igual ao ominhoca que e o charles maromba kkkkkkkkk</h2>
 }
 
 export default Home;
