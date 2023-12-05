44f1335788c09122be9bec676b43705f49a11cdd
Tony Moraes <idd_tmoraes@uolinc.com>
2023-12-04 23:37:10 -0300

    last commit

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,16 @@
 import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1+1");
-  console.log(result.rows);
-  res.status(200).json({ message: "Olha ai amigão!!! Tou ok!" });
+  const updateAt = new Date().toISOString();
+
+  res.status(200).json({
+    updated_at: updateAt,
+    database: {
+      version: 11.19,
+      max_connections: 500,
+      count_connections: 59,
+    },
+  });
 }
 
 export default status;
