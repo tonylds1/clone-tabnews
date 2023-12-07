d96e6896a4a47e46a27c81e5318fd7cd08e7cb27
Tony Edson Torres de Morais <tonylds1@gmail.com>
2023-12-07 02:06:16 +0000

    change the sentence

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 function Home() {
   return (
     <h2>
-      indo ali e um meme famoso mas o capitão limpesa e melhor porque e um super
-      heroy igual ao ominhoca que e o charles maromba kkkkkkkkk
+      Hey Carson and Steve!
     </h2>
   );
 }
